a2bc07badadf8ad61f06b1c3ee19490250fb5b38
davi costa <40581690+worthdavi@users.noreply.github.com>
2021-04-23 11:18:20 -0300

    separando elementos do index.php com include

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Editorial by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+                            <?php include_once("layout/main/header.php") ?>
+
+							<!-- Banner -->
+                            <?php include_once("layout/main/banner.php") ?>
+
+							<!-- Section -->
+                            <?php include_once("layout/main/feature_section.php") ?>
+
+							<!-- Section -->
+                            <?php include_once("layout/main/thumb_section.php") ?>							
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner">
+
+							<!-- Search -->
+                            <?php include_once("layout/sidebar/search.php") ?>
+
+							<!-- Menu -->
+                            <?php include_once("layout/sidebar/menu.php") ?>
+								
+
+							<!-- Section -->
+                            <?php include_once("layout/sidebar/thumb_section.php") ?>
+
+							<!-- Section -->
+                            <?php include_once("layout/sidebar/contact_section.php") ?>
+
+							<!-- Footer -->
+                            <?php include_once("layout/sidebar/footer.php") ?>
+
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
